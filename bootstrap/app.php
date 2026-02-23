ce5e6bf891899181459ae6f59b4edb50445d2dfe
Miftah <miftafree3@gmail.com>
2026-02-23 10:40:39 +0700

    feat: Register Stripe webhook controller in application

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,10 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => PermissionMiddleware::class,
             'role_or_permission' => RoleOrPermissionMiddleware::class,
         ]);
+
+        $middleware->validateCsrfTokens(except: [
+            'webhooks/stripe',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
